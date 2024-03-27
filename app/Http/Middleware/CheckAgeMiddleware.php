04c27bab2eccc21f7abf587aaf926552ccf093e9
Volodymyr Honcharov <ladimirvm87@gmail.com>
2024-03-27 16:55:10 +0200

    Middleware. CheckAgeMiddleware.

---
+++ app/Http/Middleware/CheckAgeMiddleware.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class CheckAgeMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        $age = $request->route('age');
+
+        if ($age < 18) {
+            return response("You must be 18 years or older ", 403);
+        }
+        return $next($request);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+//% assigning the middleware to route:
+Route::get('/restricted/{age}', function ($age) {
+    return "You are allowed to access this page. Your age is: $age";
+})->middleware('checkAgeMiddleware');
