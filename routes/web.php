aabc5eb532950da2d046f58e3093f9c758cf385a
Volodymyr Honcharov <ladimirvm87@gmail.com>
2024-04-02 21:08:44 +0300

    helpers

--- routes/web.php
+++ routes/web.php
@@ -11,3 +11,9 @@ Route::get('/', function () {
 Route::get('/restricted/{age}', function ($age) {
     return "You are allowed to access this page. Your age is: $age";
 })->middleware('checkAgeMiddleware');
+
+
+// Use custom helper function to generate random string
+Route::get('/generate-random-string', function() {
+    return generateRandomString(25);
+});
