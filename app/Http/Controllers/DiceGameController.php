8b98c3a86873e3a31897f68daa56e33ebe146ed2
Volodymyr Honcharov <ladimirvm87@gmail.com>
2024-06-28 21:15:52 +0300

    Dice game - remove commented code

--- app/Http/Controllers/DiceGameController.php
+++ app/Http/Controllers/DiceGameController.php
@@ -11,19 +11,6 @@ class DiceGameController extends Controller
         return view('dice-game.index');
     }
 
-    // public function roll(Request $request)
-    // {
-    //     $diceRoll1 = 'storage/dice/dice-six-faces-' . rand(1, 6) . '.png';
-    //     $diceRoll2 = 'storage/dice/dice-six-faces-' . rand(1, 6) . '.png';
-    //     $diceRoll3 = 'storage/dice/dice-six-faces-' . rand(1, 6) . '.png';
-    //     $diceRoll4 = 'storage/dice/dice-six-faces-' . rand(1, 6) . '.png';
-    //     $diceRoll5 = 'storage/dice/dice-six-faces-' . rand(1, 6) . '.png';
-
-    //     $diceRoll = [$diceRoll1, $diceRoll2, $diceRoll3, $diceRoll4, $diceRoll5];
-
-    //     return view('dice-game.index', ['diceRoll' => $diceRoll]);
-    // }
-
     public function roll(Request $request)
     {
         $diceRolls = [];
@@ -40,169 +27,60 @@ class DiceGameController extends Controller
         return view('dice-game.index', ['diceRolls' => $diceRolls, 'diceImages' => $diceImages, 'score' => $score]);
     }
 
-    // private function calculateScore($diceRolls)
-    // {
-    //     $counts = array_count_values($diceRolls);
-    //     $score = 0;
+     private function calculateScore($diceRolls)
+    {
+        sort($diceRolls);
+        $counts = array_count_values($diceRolls);
+        $score = 0;
 
-    //     // Calculate score for specific combinations
-    //     if (array_values($counts) === [1, 1, 1, 1, 1]) {
-    //         if (in_array(1, $diceRolls) && in_array(5, $diceRolls)) {
-    //             $score += 125;
-    //         } elseif (in_array(2, $diceRolls) && in_array(6, $diceRolls)) {
-    //             $score += 250;
-    //         }
-    //     }
-
-    //     foreach ($counts as $num => $count) {
-    //         switch ($num) {
-    //             case 1:
-    //                 if ($count == 3) $score += 100;
-    //                 if ($count == 4) $score += 200;
-    //                 if ($count == 5) $score += 1000;
-    //                 break;
-    //             case 2:
-    //                 if ($count == 3) $score += 20;
-    //                 if ($count == 4) $score += 40;
-    //                 if ($count == 5) $score += 200;
-    //                 break;
-    //             case 3:
-    //                 if ($count == 3) $score += 30;
-    //                 if ($count == 4) $score += 60;
-    //                 if ($count == 5) $score += 300;
-    //                 break;
-    //             case 4:
-    //                 if ($count == 3) $score += 40;
-    //                 if ($count == 4) $score += 80;
-    //                 if ($count == 5) $score += 400;
-    //                 break;
-    //             case 5:
-    //                 if ($count == 1) $score += 5;
-    //                 if ($count == 3) $score += 50;
-    //                 if ($count == 4) $score += 100;
-    //                 if ($count == 5) $score += 500;
-    //                 break;
-    //             case 6:
-    //                 if ($count == 3) $score += 60;
-    //                 if ($count == 4) $score += 120;
-    //                 if ($count == 5) $score += 600;
-    //                 break;
-    //         }
-    //     }
-
-    //     return $score;
-    // }
-
-    // private function calculateScore($diceRolls)
-    // {
-    //     $counts = array_count_values($diceRolls);
-    //     $score = 0;
-
-    //     // Calculate score for specific combinations
-    //     if (array_values($counts) === [1, 1, 1, 1, 1]) {
-    //         if (in_array(1, $diceRolls) && in_array(5, $diceRolls)) {
-    //             $score += 150;
-    //         } elseif (in_array(2, $diceRolls) && in_array(6, $diceRolls)) {
-    //             $score += 250;
-    //         }
-    //     }
-
-    //     foreach ($counts as $num => $count) {
-    //         switch ($num) {
-    //             case 1:
-    //                 if ($count == 1) $score += 10;
-    //                 if ($count == 2) $score += 20;
-    //                 if ($count == 3) $score += 100;
-    //                 if ($count == 4) $score += 200;
-    //                 if ($count == 5) $score += 1000;
-    //                 break;
-    //             case 2:
-    //                 if ($count == 3) $score += 20;
-    //                 if ($count == 4) $score += 40;
-    //                 if ($count == 5) $score += 200;
-    //                 break;
-    //             case 3:
-    //                 if ($count == 3) $score += 30;
-    //                 if ($count == 4) $score += 60;
-    //                 if ($count == 5) $score += 300;
-    //                 break;
-    //             case 4:
-    //                 if ($count == 3) $score += 40;
-    //                 if ($count == 4) $score += 80;
-    //                 if ($count == 5) $score += 400;
-    //                 break;
-    //             case 5:
-    //                 if ($count == 1) $score += 5;
-    //                 if ($count == 2) $score += 10; // Adding this to ensure pairs of 5 are accounted for
-    //                 if ($count == 3) $score += 50;
-    //                 if ($count == 4) $score += 100;
-    //                 if ($count == 5) $score += 500;
-    //                 break;
-    //             case 6:
-    //                 if ($count == 3) $score += 60;
-    //                 if ($count == 4) $score += 120;
-    //                 if ($count == 5) $score += 600;
-    //                 break;
-    //         }
-    //     }
-
-    //     return $score;
-    // }
-
-    private function calculateScore($diceRolls)
-{
-    sort($diceRolls);
-    $counts = array_count_values($diceRolls);
-    $score = 0;
-
-    // Check for special combinations first
-    if ($diceRolls == [1, 2, 3, 4, 5]) {
-        $score += 150;
-    } elseif ($diceRolls == [2, 3, 4, 5, 6]) {
-        $score += 250;
-    } else {
-        // Calculate score for other combinations
-        foreach ($counts as $num => $count) {
-            switch ($num) {
-                case 1:
-                    if ($count == 1) $score += 10;
-                    if ($count == 2) $score += 20;
-                    if ($count == 3) $score += 100;
-                    if ($count == 4) $score += 200;
-                    if ($count == 5) $score += 1000;
-                    break;
-                case 2:
-                    if ($count == 3) $score += 20;
-                    if ($count == 4) $score += 40;
-                    if ($count == 5) $score += 200;
-                    break;
-                case 3:
-                    if ($count == 3) $score += 30;
-                    if ($count == 4) $score += 60;
-                    if ($count == 5) $score += 300;
-                    break;
-                case 4:
-                    if ($count == 3) $score += 40;
-                    if ($count == 4) $score += 80;
-                    if ($count == 5) $score += 400;
-                    break;
-                case 5:
-                    if ($count == 1) $score += 5;
-                    if ($count == 2) $score += 10; // Adding this to ensure pairs of 5 are accounted for
-                    if ($count == 3) $score += 50;
-                    if ($count == 4) $score += 100;
-                    if ($count == 5) $score += 500;
-                    break;
-                case 6:
-                    if ($count == 3) $score += 60;
-                    if ($count == 4) $score += 120;
-                    if ($count == 5) $score += 600;
-                    break;
+        // Check for special combinations first
+        if ($diceRolls == [1, 2, 3, 4, 5]) {
+            $score += 150;
+        } elseif ($diceRolls == [2, 3, 4, 5, 6]) {
+            $score += 250;
+        } else {
+            // Calculate score for other combinations
+            foreach ($counts as $num => $count) {
+                switch ($num) {
+                    case 1:
+                        if ($count == 1) $score += 10;
+                        if ($count == 2) $score += 20;
+                        if ($count == 3) $score += 100;
+                        if ($count == 4) $score += 200;
+                        if ($count == 5) $score += 1000;
+                        break;
+                    case 2:
+                        if ($count == 3) $score += 20;
+                        if ($count == 4) $score += 40;
+                        if ($count == 5) $score += 200;
+                        break;
+                    case 3:
+                        if ($count == 3) $score += 30;
+                        if ($count == 4) $score += 60;
+                        if ($count == 5) $score += 300;
+                        break;
+                    case 4:
+                        if ($count == 3) $score += 40;
+                        if ($count == 4) $score += 80;
+                        if ($count == 5) $score += 400;
+                        break;
+                    case 5:
+                        if ($count == 1) $score += 5;
+                        if ($count == 2) $score += 10; // Adding this to ensure pairs of 5 are accounted for
+                        if ($count == 3) $score += 50;
+                        if ($count == 4) $score += 100;
+                        if ($count == 5) $score += 500;
+                        break;
+                    case 6:
+                        if ($count == 3) $score += 60;
+                        if ($count == 4) $score += 120;
+                        if ($count == 5) $score += 600;
+                        break;
+                }
             }
         }
-    }
 
-    return $score;
-}
+        return $score;
+    }
 
 }
